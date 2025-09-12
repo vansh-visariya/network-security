0ba29a356e623ecf22edd2fcd217be307a31f8c2
vansh visariya <vanshvisariya1985@gmail.com>
2025-09-12 22:50:53 +0530

    modify file system

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 
 from network_security.utils.main_utils.utils import load_object
+from network_security.utils.ml_utils.model.estimator import NetworkModel
 from network_security.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME
 
 ca = certifi.where()  ##It ensures that your Python application uses a trusted and up-to-date certificate authority (CA) bundle, rather than relying on potentially outdated or missing system certificates.
@@ -36,6 +37,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./template")
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -47,7 +51,27 @@ async def train_route():
         training_pipeline.run_pipeline()
         return Response("Training successful !!")
     except Exception as e:
-        return Response(f"Error Occurred! {e}")
+       raise networkseacurityException(e,sys)
+    
+@app.post("/predict")
+async def predict_route(request:Request,file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+
+        model = load_object("artifacts/08_28_2025_21_30_12/model_trainer/trained_model/model.pkl")
+        preprocessor = load_object("artifacts/08_28_2025_21_30_12/data_transformation/transformed_object/preprocessed_object.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor,model=model.model,param=model.param)
+
+        prediction = network_model.predict(df)
+        df['predicted_column'] = prediction
+        
+        os.makedirs("prediction_output", exist_ok=True) 
+        df.to_csv("prediction_output/output.csv")
+
+        table_html = df.to_html(classes='table table-striped')
+        return templates.TemplateResponse("table.html", {"request": request, "table":table_html})
+    except Exception as e:
+        raise networkseacurityException(e,sys)
     
 if __name__ == "__main__":
     app_run(app, host="localhost", port=8080)
